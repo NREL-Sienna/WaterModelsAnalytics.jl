170eb69932929e81a9488844424f48f1003e34e4
Byron Tasseff <btasseff@lanl.gov>
2020-07-18 16:29:12 -0500

    Tidying up module.

--- src/WaterModelsAnalytics.jl
+++ src/WaterModelsAnalytics.jl
@@ -2,34 +2,34 @@
 Module for visualizing water networks, and potentially other graphics
 """
 module WaterModelsAnalytics
+    # imports
+    import WaterModels
+    const _WM = WaterModels
 
-# imports
-import WaterModels
-# importing only the viridis color scheme for now; might allow the color scheme to be
-# selected by the user at some point, in which case will need to import all of ColorShemes
-import ColorSchemes.viridis
-import ColorTypes.HSV
-import Statistics.mean
-using Printf
+    # importing only the viridis color scheme for now; might allow the color scheme to be
+    # selected by the user at some point, in which case will need to import all of ColorShemes
+    import ColorSchemes.viridis
+    import ColorTypes.HSV
+    import Statistics.mean
+    using Printf
 
-import Plots
+    import Plots
 
-# python imports
-import PyCall
-import PyCall.PyObject
-const nx = PyCall.PyNULL()
-function __init__()
-    copy!(nx, PyCall.pyimport("networkx"))
+    # python imports
+    import PyCall
+    import PyCall.PyObject
+    const nx = PyCall.PyNULL()
+    function __init__()
+        copy!(nx, PyCall.pyimport("networkx"))
+    end
+
+    # includes
+    include("graph/common.jl")
+
+    # exports
+    export build_graph
+    export write_dot
+    export run_dot
+    export colorbar
+    export write_visualization
 end
-
-# includes
-include("graph/common.jl")
-
-# exports
-export build_graph
-export write_dot
-export run_dot
-export colorbar
-export write_visualization
-
-end # module
