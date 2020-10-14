533422b47804aa56ab580ca5a99b9143cd19daab
Jonathan Stickel <jonathan.stickel@nrel.gov>
2020-10-14 15:27:44 -0600

    add keyword switche for displaying and saving the plot

--- src/plots/pumps.jl
+++ src/plots/pumps.jl
@@ -1,10 +1,9 @@
-# TODO:
-# - make sure that single-point head curves are managed OK
-
 """
-Plot the normalized pump curves for the pumps in a network
+Plot the normalized pump curves for the pumps in a network. Displays the plot on the screen
+by default. Use keywords `screen` and `savepath` to control whether to display and/or save
+to file.
 """
-function plot_pumps(pumps::Dict{String,Any})
+function plot_pumps(pumps::Dict{String,Any}; screen=true, savepath=nothing)
     m = 50
     Qhat = Array(range(0.0, 2.0, length=m))
     etahat = -Qhat.^2 .+ 2*Qhat
@@ -16,7 +15,6 @@ function plot_pumps(pumps::Dict{String,Any})
     p3 = Plots.plot()
     plotargs = Dict(:lt=>:scatter, :shape=>:circle, :ms=>6, :msc=>:auto)
     for (key,pump) in pumps
-        #println(pump["name"])
         Qbep = pump["q_bep"]
         Gbep = pump["g_bep"]
         Pbep = pump["P_bep"] # inconsistent capitilization...
@@ -32,16 +30,27 @@ function plot_pumps(pumps::Dict{String,Any})
             qmin = head[1,1]/Qbep
             qmax = head[end,1]/Qbep
         end
+
         # create power curves by linear interpolation of head and eff curves
-        headitp = _ITP.LinearInterpolation(head[:,1]/Qbep, head[:,2]/Gbep)
+        if size(head)[1] > 1
+            headitp = _ITP.LinearInterpolation(head[:,1]/Qbep, head[:,2]/Gbep)
+        else
+            headitp(x::Float64) = head[1,2]
+            headitp(x::Array{Float64}) = head[1,2]*ones(size(x))
+        end
         if eff_is_curve
             effitp = _ITP.LinearInterpolation(eff[:,1]/Qbep, eff[:,2]/etabep)
         else
             effitp(x::Float64) = eff
             effitp(x::Array{Float64}) = eff*ones(size(x))
         end
-        q = Array(range(qmin, stop=qmax, length=7)) # can change number of points here
-        pow = headitp(q).*q./effitp(q)
+        if qmin==qmax
+            q = [1]
+            pow = [1]
+        else
+            q = Array(range(qmin, stop=qmax, length=7)) # can change number of points here
+            pow = headitp(q).*q./effitp(q)
+        end
 
         Plots.plot!(p1, head[:,1]/Qbep, head[:,2]/Gbep, label=""; plotargs...)
         if eff_is_curve
@@ -57,11 +66,10 @@ function plot_pumps(pumps::Dict{String,Any})
                 legend=:bottom)
     Plots.plot!(p3, Qhat, Phat, lc=:black, lw=2, label="", xlabel=L"\hat{Q}",
                 ylabel=L"\hat{P}")
-    #height = 400  # default is 600 x 400 (w x h)
-    #width = 4*height
     width = 400
     height = width*4/6*3
-    ## this command must be last to display the plot?
-    #Plots.plot(p1, p2, p3, size=(width,height), layout=(1,3))
-    Plots.plot(p1, p2, p3, size=(width,height), layout=(3,1)) # orient 3x1 instead to share
+    pfig = Plots.plot(p1, p2, p3, size=(width,height), layout=(3,1));
+    # display on the screen and/or save
+    screen ? display(pfig) : nothing
+    savepath != nothing ? Plots.savefig(pfig, savepath) : nothing
 end 
