063f8647529c215570674d197722133b72705122
enzoliuyang94 <enzoliu128@gmail.com>
2020-11-06 11:27:38 -0800

    saves temporary inp files to a temporary folder that is deleted after simulation

--- src/analysis/simulation.jl
+++ src/analysis/simulation.jl
@@ -4,6 +4,8 @@ Perform EPANET hydraulic simulation (via WNTR) using controls in WM
 wm_solution. 
 """
 
+using Random
+
 function simulate(wm_data::Dict{String,Any}, wm_solution::Dict{String,Any},
                      inpfilepath::String)
 
@@ -181,6 +183,11 @@ function simulate(wm_data::Dict{String,Any}, wm_solution::Dict{String,Any},
 
     # WNTR simulation 
     wns = wntr.sim.EpanetSimulator(wn)
-    wnres = wns.run_sim()
+    path_to_tmp_folder = string(@__DIR__)*"/"*Random.randstring(5)
+    tmp_file = Random.randstring(5)
+    mkdir(path_to_tmp_folder)
+    wnres = wns.run_sim(path_to_tmp_folder*"/"*tmp_file)
+    rm(path_to_tmp_folder,recursive=true)
+
     return wn,wnres
 end 
