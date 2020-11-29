5f1911396b187aacae3ad47e944aaf431c50c914
Jonathan Stickel <jonathan.stickel@nrel.gov>
2020-11-29 08:52:17 -0700

    flush out python- and wntr-based visualization

--- src/python/wntr_vis.py
+++ src/python/wntr_vis.py
@@ -3,21 +3,19 @@ Functions to create graph visualizations of WNTR networks
 """
 
 ## TODO:
-# * reduce digits of base demand printing
-# * reservoir head: use reservoir head pattern and multiply by base elevation
-# - process shutoff valves
+# - display the length of links
+# - display information from the network solution
+# - process shutoff valves; rarely occur in networks we are looking to use...
 
 import numpy as np
 import wntr
 import networkx as nx
-import pygraphviz as pgv
+#import pygraphviz as pgv
 import pandas as pd 
 import warnings
-
-from matplotlib import cm, colors
-    
-#from matplotlib import pyplot as plt
-#ion()
+import matplotlib
+from matplotlib import cm, colors, colorbar, pyplot
+import subprocess
 
 
 def build_graph(wn, time=1, wnsol=None):
@@ -26,8 +24,8 @@ def build_graph(wn, time=1, wnsol=None):
 
     `time` is presumed to be integer hours
 
+    `wnsol` is currently ignored [TBD]
     """    
-    # wn_dict = wn.todict() # not used...
 
     # # TBD: use the simulation results
     # link_results = wnsol.link
@@ -35,7 +33,6 @@ def build_graph(wn, time=1, wnsol=None):
 
     # create the graph
     Gnx = wn.get_graph() # networkx object; more features and functions
-    #Gpg = nx.nx_agraph.to_agraph(G) # pygraphviz object--use later     
 
     # node position attributes (coordinates in inp file)
     pos = nx.get_node_attributes(Gnx, "pos")
@@ -81,18 +78,31 @@ def build_graph(wn, time=1, wnsol=None):
         node = wn.get_node(nname)
         pat = node.demand_timeseries_list.pattern_list()[0]
         demval = based[nname]*pat[time-1]
-        Gnx.nodes[nname]['label'] = nname + "\nd = %g" % demval
+        Gnx.nodes[nname]['label'] = nname + "\nd = %2.2g" % demval
 
     ## add elevation information
     # presume that junctions and tanks have an "elevation" and reservoirs have a
     # "base head" (and not keys for both)
     elevs = wn.query_node_attribute('elevation')
-    # I think this next line gets the base head for reservoirs---using it by itself
-    # is useless if there is a pattern associated with the reservoir: TBD - use the
-    # time and the pattern and multiply by the base head, JJS 1/28/19
-    elevs = pd.concat((elevs, wn.query_node_attribute('base_head')))
+    # use the time and the pattern and multiply by the base head
+    res_elevs = wn.query_node_attribute('base_head')
+    resnames = res_elevs.index.values
+    for node in resnames:
+        res = wn.get_node(node)
+        mults = res.head_timeseries.pattern.multipliers
+        # "base_value" should be the same as what was aleady queried by
+        # "base_head", so either should work
+        res_elevs[node] = res.head_timeseries.base_value * mults[time-1]
+    
+    elevs = pd.concat((elevs, res_elevs))
+    elmin = elevs.min()
+    elmax = elevs.max()
+    # store max and min elevations as graph attributes to use for creating a colorbar
+    Gnx.graph["elmin"] = elmin
+    Gnx.graph["elmax"] = elmax
+    
     nodenames = elevs.index.values
-    elevsrel = (elevs - elevs.min())/(elevs.max()-elevs.min())
+    elevsrel = (elevs - elmin)/(elmax - elmin)
     cmap = cm.get_cmap('viridis') # can make colormap a user option
     elevclrs = pd.DataFrame(colors.rgb_to_hsv(cmap(elevsrel)[:,:3]), index=nodenames)
     for node in nodenames:
@@ -132,31 +142,58 @@ def write_graph(G, filename, layout="dot"):
         warnings.warn("%s is not a supported layout; dot was used instead"%layout)
     
 
-#     # use ghostscript to make a 2-page pdf with the colorbar as the first page
-#     # -- I'd like to embed the colorbar in the graph page, but I don't know how
-#     # to do that yet, JJS 10/10/19
-#     command = "gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -sOutputFile=" + outputfilebase + "_w_cb.pdf " + cbfile + " " + pdffile 
-#     subprocess.run(command.split())
-#     # remove support files -- provide an option to keep these !!!!!!!
-#     command = "rm " + gvfile + " " + cbfile + " " + pdffile
-#     subprocess.run(command.split())
+def write_cbar(G, filename):
+    """
+    make the colorbar for the elevations 
+    """
+    # https://matplotlib.org/tutorials/colors/colorbar_only.html
+    # G.graph_attr.keys() # to see the attribute keys
+
+    # if user's matplotlib environment is interactive, turn off
+    interactive = matplotlib.is_interactive()
+    if interactive:
+        pyplot.ioff()
+
+    fig = pyplot.figure(figsize=(6,1))
+    ax = pyplot.gca()
+    cmap = cm.viridis
+    norm = colors.Normalize(vmin=G.graph_attr["elmin"], vmax=G.graph_attr["elmax"])
+    cb1 = colorbar.ColorbarBase(ax, cmap=cmap, norm=norm, orientation='horizontal')
+    cb1.set_label('Elevation [m]')
+    pyplot.savefig(filename, bbox_inches='tight')
+    pyplot.close(fig) # may not be necessary if not displaying the figure?
+    # if user's matplotlib environment was interactive, turn back on
+    if interactive:
+        pyplot.ion()
+
+
+def write_visualization(wn, basefilename, time=1, wnsol=None, layout="dot",
+                        del_files=True):
+    """
+    Write out to a file a visualization for a WaterModels network dictionary
+    parsed from an EPANET file. `basefilename` should not include an extension
+    and will be appended with `_w_cb.pdf` in the final output file, which is a
+    multi-page PDF. The `layout` option equates to the layout functions of
+    graphviz (dot, neato, etc.). Use `del_files=False` to keep the intermediate
+    files.
+
+    `time` is presumed to be integer hours; `wnsol` is currently ignored [TBD]
+    """
+    # use ghostscript to make a 2-page pdf with the colorbar as the first page
+    # -- I'd like to embed the colorbar in the graph page, but I don't know how
+    # to do that yet, JJS 10/10/19
+    # maybe this will do it?:  https://github.com/mstamy2/PyPDF2
+    graphfile = basefilename + "_graph.pdf"
+    cbfile = basefilename + "_cbar.pdf"
+    outfile = basefilename + "_w_cb.pdf"
+
+    G = build_graph(wn, time, wnsol)
+    write_graph(G, graphfile, layout)
+    write_cbar(G, cbfile)
     
-
-
-
-    # #### put this in a separate function
-    # # make the colorbar for the elevations -- not sure how best to integrate this with the graph
-    # # https://matplotlib.org/tutorials/colors/colorbar_only.html
-    # from matplotlib import colorbar
-
-    # figure(1, figsize=(6,1))
-    # clf()
-    # ax = gca()
-    # cmap = cm.viridis
-    # norm = colors.Normalize(vmin=elevs.min(), vmax=elevs.max())
-    # cb1 = colorbar.ColorbarBase(ax, cmap=cmap, norm=norm, orientation='horizontal')
-    # cb1.set_label('Elevation [m]')
-    # if graphoutput:
-    #     cbfile = outputfilebase + "_colorbar.pdf"
-    #     # something weird is happening with this in python 3.8 -- see if it persists, JJS 8/21/20
-    #     savefig(cbfile, bbox_inches='tight')
+    command = "gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -sOutputFile=" + outfile + " " + cbfile + " " + graphfile 
+    subprocess.run(command.split())
+    if del_files:
+        # remove support files
+        command = "rm " + cbfile + " " + graphfile
+        subprocess.run(command.split())
