66411a6fc5729edfcafb0fcf97a6b66cb0d8e283
Byron Tasseff <btasseff@lanl.gov>
2021-02-04 21:00:42 -0700

    Remove print statements.

--- test/graph.jl
+++ test/graph.jl
@@ -2,7 +2,6 @@
     @testset "write_visualization" begin
         data = _WM.parse_file("$(wm_path)/examples/data/epanet/van_zyl.inp")
         tmp_directory = mktempdir()
-        println(tmp_directory)
         write_visualization(data, joinpath(tmp_directory, "van_zyl"), del_files = false)
         @test isfile(joinpath(tmp_directory, "van_zyl_graph.pdf"))
         @test isfile(joinpath(tmp_directory, "van_zyl_cbar.pdf"))
