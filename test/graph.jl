18a69ef7e8c784f0b3a6fafd022f67e458c630e9
Byron Tasseff <btasseff@lanl.gov>
2021-02-04 17:19:12 -0700

    Check if build log exists before trying to throw warnings.

--- test/graph.jl
+++ test/graph.jl
@@ -1,11 +1,11 @@
 @testset "src/graph/common.jl" begin
     @testset "build_graph" begin
         data = _WM.parse_file("$(wm_path)/examples/data/epanet/van_zyl.inp")
-        tmppth = mktempdir()
-        write_visualization(data, "$(tmppth)/van_zyl_wm", del_files=false)
-        @test isfile("$(tmppth)/van_zyl_wm_graph.pdf")
-        @test isfile("$(tmppth)/van_zyl_wm_cbar.pdf")
-        @test isfile("$(tmppth)/van_zyl_wm_graph_w_cb.pdf")
-        rm(tmppth, recursive=true)
+        tmp_directory = mktempdir()
+        write_visualization(data, "$(tmp_directory)/van_zyl", del_files = false)
+        @test isfile("$(tmp_directory)/van_zyl_graph.pdf")
+        @test isfile("$(tmp_directory)/van_zyl_cbar.pdf")
+        @test isfile("$(tmp_directory)/van_zyl_w_cb.pdf")
+        rm(tmppth, recursive = true)
     end
 end
