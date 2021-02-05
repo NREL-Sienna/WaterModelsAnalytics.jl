b6847a55b158b2ff29b55f46b59bd6430cc844ad
Byron Tasseff <btasseff@lanl.gov>
2021-02-04 20:26:24 -0700

    Try to fix CI tests.

--- test/graph.jl
+++ test/graph.jl
@@ -1,11 +1,12 @@
 @testset "src/graph/common.jl" begin
-    @testset "build_graph" begin
+    @testset "write_visualization" begin
         data = _WM.parse_file("$(wm_path)/examples/data/epanet/van_zyl.inp")
         tmp_directory = mktempdir()
-        write_visualization(data, "$(tmp_directory)/van_zyl", del_files = false)
-        @test isfile("$(tmp_directory)/van_zyl_graph.pdf")
-        @test isfile("$(tmp_directory)/van_zyl_cbar.pdf")
-        @test isfile("$(tmp_directory)/van_zyl_w_cb.pdf")
+        println(tmp_directory)
+        write_visualization(data, joinpath(tmp_directory, "van_zyl"), del_files = false)
+        @test isfile(joinpath(tmp_directory, "van_zyl_graph.pdf"))
+        @test isfile(joinpath(tmp_directory, "van_zyl_cbar.pdf"))
+        @test isfile(joinpath(tmp_directory, "van_zyl_w_cb.pdf"))
         rm(tmp_directory, recursive = true)
     end
 end
